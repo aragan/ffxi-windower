6b0c6508dd22cab4bb958ac938fd39bfc74ac1dd
aragan <64033062+aragan@users.noreply.github.com>
2024-03-20 05:06:23 +0300

    Add files via upload

---
+++ LargeAddressAwarePatcher/Source/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace LargeAddressAwarePatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ui_FilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ui_Dialog = new System.Windows.Forms.OpenFileDialog();
+            this.ui_Browse = new System.Windows.Forms.Button();
+            this.ui_Patch = new System.Windows.Forms.Button();
+            this.ui_Unpatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ui_FilePath
+            // 
+            this.ui_FilePath.Location = new System.Drawing.Point(44, 12);
+            this.ui_FilePath.Name = "ui_FilePath";
+            this.ui_FilePath.Size = new System.Drawing.Size(356, 20);
+            this.ui_FilePath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File:";
+            // 
+            // ui_Dialog
+            // 
+            this.ui_Dialog.DefaultExt = "exe";
+            this.ui_Dialog.FileName = "openFileDialog1";
+            this.ui_Dialog.Filter = "Executable Files|*.exe";
+            this.ui_Dialog.Title = "Select file to patch.";
+            // 
+            // ui_Browse
+            // 
+            this.ui_Browse.Location = new System.Drawing.Point(406, 10);
+            this.ui_Browse.Name = "ui_Browse";
+            this.ui_Browse.Size = new System.Drawing.Size(75, 23);
+            this.ui_Browse.TabIndex = 2;
+            this.ui_Browse.Text = "Browse";
+            this.ui_Browse.UseVisualStyleBackColor = true;
+            this.ui_Browse.Click += new System.EventHandler(this.ui_Browse_Click);
+            // 
+            // ui_Patch
+            // 
+            this.ui_Patch.Location = new System.Drawing.Point(144, 38);
+            this.ui_Patch.Name = "ui_Patch";
+            this.ui_Patch.Size = new System.Drawing.Size(75, 23);
+            this.ui_Patch.TabIndex = 3;
+            this.ui_Patch.Text = "Patch";
+            this.ui_Patch.UseVisualStyleBackColor = true;
+            this.ui_Patch.Click += new System.EventHandler(this.ui_Patch_Click);
+            // 
+            // ui_Unpatch
+            // 
+            this.ui_Unpatch.Location = new System.Drawing.Point(225, 38);
+            this.ui_Unpatch.Name = "ui_Unpatch";
+            this.ui_Unpatch.Size = new System.Drawing.Size(75, 23);
+            this.ui_Unpatch.TabIndex = 4;
+            this.ui_Unpatch.Text = "Unpatch";
+            this.ui_Unpatch.UseVisualStyleBackColor = true;
+            this.ui_Unpatch.Click += new System.EventHandler(this.ui_Unpatch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 73);
+            this.Controls.Add(this.ui_Unpatch);
+            this.Controls.Add(this.ui_Patch);
+            this.Controls.Add(this.ui_Browse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ui_FilePath);
+            this.Name = "Form1";
+            this.Text = "LargeAddressAwarePatcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ui_FilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog ui_Dialog;
+        private System.Windows.Forms.Button ui_Browse;
+        private System.Windows.Forms.Button ui_Patch;
+        private System.Windows.Forms.Button ui_Unpatch;
+    }
+}
+
---
+++ LargeAddressAwarePatcher/Source/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LargeAddressAwarePatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ui_Browse_Click(object sender, EventArgs e)
+        {
+            DialogResult Result = ui_Dialog.ShowDialog();
+            if (Result == DialogResult.OK)
+            {
+                ui_FilePath.Text = ui_Dialog.FileName;
+            }
+        }
+
+        private void ui_Patch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var stream = File.Open(ui_FilePath.Text, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    var reader = new BinaryReader(stream);
+                    ushort mzHeader = reader.ReadUInt16();
+                    if (mzHeader != 0x5A4D)
+                    {
+                        MessageBox.Show("No MZ header found.  Failed to patch.");
+                        return;
+                    }
+
+                    reader.BaseStream.Position = 0x3C;
+                    uint peOffset = reader.ReadUInt32();
+
+                    reader.BaseStream.Position = peOffset;
+                    if (reader.ReadUInt32() != 0x4550)
+                    {
+                        MessageBox.Show("No PE header found.  Failed to patch.");
+                        return;
+                    }
+
+                    long flagAddress = reader.BaseStream.Position + 0x12;
+                    reader.BaseStream.Position = flagAddress;
+
+                    ushort flags = reader.ReadUInt16();
+                    flags |= 0x20;
+
+                    stream.Position = flagAddress;
+                    stream.Write(BitConverter.GetBytes(flags), 0, 2);
+                    reader.Close();
+                    MessageBox.Show("Patch succeeded!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("An error occured.  Failed to patch.");
+            }
+        }
+
+        private void ui_Unpatch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var stream = File.Open(ui_FilePath.Text, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    var reader = new BinaryReader(stream);
+                    ushort mzHeader = reader.ReadUInt16();
+                    if (mzHeader != 0x5A4D)
+                    {
+                        MessageBox.Show("No MZ header found.  Failed to unpatch.");
+                        return;
+                    }
+
+                    reader.BaseStream.Position = 0x3C;
+                    uint peOffset = reader.ReadUInt32();
+
+                    reader.BaseStream.Position = peOffset;
+                    if (reader.ReadUInt32() != 0x4550)
+                    {
+                        MessageBox.Show("No PE header found.  Failed to unpatch.");
+                        return;
+                    }
+
+                    long flagAddress = reader.BaseStream.Position + 0x12;
+                    reader.BaseStream.Position = flagAddress;
+
+                    ushort flags = reader.ReadUInt16();
+                    if ((flags & 0x20) != 0)
+                    {
+                        flags -= 0x20;
+                    }
+
+                    stream.Position = flagAddress;
+                    stream.Write(BitConverter.GetBytes(flags), 0, 2);
+                    reader.Close();
+                    MessageBox.Show("Unpatch succeeded!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("An error occured.  Failed to unpatch.");
+            }
+        }
+    }
+}
